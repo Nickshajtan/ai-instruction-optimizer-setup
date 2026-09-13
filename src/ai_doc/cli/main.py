2cf43ce011d0f9dc0236d100c3ff83a1eae9b079
Nikita <shajtanuch@gmail.com>
2026-09-13 18:40:51 +0300

    Register planning probe CLI command

--- src/ai_doc/cli/main.py
+++ src/ai_doc/cli/main.py
@@ -9,6 +9,7 @@ from ai_doc import __version__
 from ai_doc.cli.check import check_command
 from ai_doc.cli.doctor import doctor_command
 from ai_doc.cli.optimize import optimize_command
+from ai_doc.cli.probe import probe_command
 from ai_doc.cli.setup import setup_command
 from ai_doc.config.loader import ConfigError, write_default_config
 
@@ -51,6 +52,7 @@ def init(
 
 app.command(name="check")(check_command)
 app.command(name="optimize")(optimize_command)
+app.command(name="probe")(probe_command)
 app.command(name="doctor")(doctor_command)
 app.command(name="setup")(setup_command)
 
